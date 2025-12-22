0ede25d56a759d04279683b61bfcb3ee08bf406c
vardhan <vardhankorada25@gmail.com>
2025-12-22 21:37:17 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import dill
 
 from src.exceptions import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -14,14 +14,18 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys.exc_info())
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(models)):
             model=list(models.values())[i]
             model_name=list(models.keys())[i]
+            para=param[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
             #training the model
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             #predicting the model
